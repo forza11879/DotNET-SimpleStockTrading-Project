a5f6f15a887ab11f7ce504b54f2e25ee7cfa027a
john john <forza11879@gmail.com>
2017-04-13 14:43:58 -0400

    * HomeWindow added , chaged StockQuotesTable, changes Stock Class for new StockQuotesTable

---
+++ HomeWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TradingApp
+{
+    /// <summary>
+    /// Interaction logic for HomeWindow.xaml
+    /// </summary>
+    public partial class HomeWindow : Window
+    {
+        Database db;
+
+        public HomeWindow()
+        {
+            try
+            {
+                db = new Database();
+
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.StackTrace);
+                MessageBox.Show("Error opning database connection: " + e.Message);
+            }
+            InitializeComponent();
+            refreshStockQuotesList();
+        }
+
+        private void refreshStockQuotesList()
+        {
+
+            List<StockQuotes> StockQuotesList = db.GetAllStockQuotes();
+            lvStockQuotesList.ItemsSource = StockQuotesList;
+
+        }
+
+        private void lvStockQuotesList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (lvStockQuotesList.SelectedItem == null)
+            {
+                //if there is no selection dissable buttons Update and Add
+                btnBuy.IsEnabled = false;
+                btnSell.IsEnabled = false;
+            }
+            else
+            {
+                //if there is a selectoin enable buttons Update and Add
+                btnBuy.IsEnabled = true;
+                btnSell.IsEnabled = true;
+                //if there is a selection populate text boxes and combo box with the properties of the objetc selected in data grid
+                StockQuotes selectedStockQuotes = (StockQuotes)lvStockQuotesList.SelectedItem;
+
+                lblSymbol.Content = selectedStockQuotes.Symbol;
+                lblCompanyName.Content = selectedStockQuotes.Name;
+                tbTradingPrice.Text = selectedStockQuotes.LastTrade + "";
+
+
+            }
+        }
+    }
+
+
+
+}
+}
--- Stock.cs
+++ Stock.cs
@@ -8,19 +8,43 @@ namespace TradingApp
 {
     class Stock
     {
-        int StockID;
-        String Symbol;
-        String Name;
-        decimal AskPrice;
-        decimal BitPrice;
-
-        public Stock(int stockID, string symbol, string name, decimal askPrice, decimal bitPrice)
+        public Stock(int stockID, string symbol, string name, decimal bid, decimal ask, decimal lastTrade, decimal change, decimal changePercentage, decimal high, decimal low,
+            int volume, int marketCap, decimal high52,decimal low52)
         {
             StockID = stockID;
-            Symbol = symbol;
+            Symbol = Symbol;
             Name = name;
-            AskPrice = askPrice;
-            BitPrice = bitPrice;
+            Bid = bid;
+            Ask = ask;
+            LastTrade = lastTrade;
+            Change = change;
+            ChangePercentage = changePercentage;
+            High = high;
+            Low = low;
+            Volume = volume;
+            High52 = high52;
+            Low52 = low52;
+
+
+
         }
+
+
+        public int StockID { get; set; }
+        public string Symbol { get; set; }
+        public string Name { get; set; }
+        public decimal Bid { get; set; }
+        public decimal Ask { get; set; }
+        public decimal LastTrade { get; set; }
+        public decimal Change { get; set; }
+        public decimal ChangePercentage { get; set; }
+        public decimal High { get; set; }
+        public decimal Low { get; set; }
+        public int Volume { get; set; }
+        public int MarketCap { get; set; }
+        public decimal High52 { get; set; }
+        public decimal Low52 { get; set; }
+
+        
     }
 }
