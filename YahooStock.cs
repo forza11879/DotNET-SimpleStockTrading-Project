14c3d17147610568927712cce324d9385b72fabe
MSI\John <forza11879@gmail.com>
2017-04-19 22:36:18 -0400

    *trim update

--- YahooStock.cs
+++ YahooStock.cs
@@ -11,10 +11,10 @@ namespace TradingApp
     {
 
 
-            //Lets Prase CSVdata to list of objects
+        //Lets Prase CSVdata to list of objects
 
-            // caller must handle ParseException
-            public static List<YahooStock> Parse(string csvData)
+        // caller must handle ParseException
+        public static List<YahooStock> Parse(string csvData)
         {
             List<YahooStock> StockFromApiList = new List<YahooStock>();
             string[] rows = csvData.Replace("\r", "").Split('\n');
@@ -31,20 +31,21 @@ namespace TradingApp
                 try
                 {
                     //s.Symbol = Convert.ToString(cols[0]).TrimStart('"');
-                    s.Symbol = Convert.ToString(cols[0]).TrimEnd('"');
-                    s.Name = cols[1];
-                s.Bid = (cols[2].ToUpper().Contains("N/A")) ? (decimal?) null : Convert.ToDecimal(cols[2]);
-                s.Ask = (cols[3].ToUpper().Contains("N/A")) ? (decimal?)null : Convert.ToDecimal(cols[3]);
-                s.Open = (cols[4].ToUpper().Contains("N/A")) ? (decimal?)null : Convert.ToDecimal(cols[4]);
-                s.PreviousClose = (cols[5].ToUpper().Contains("N/A")) ? (decimal?)null : Convert.ToDecimal(cols[5]);
-                s.LastTrade = (cols[6].ToUpper().Contains("N/A")) ? (decimal?)null : Convert.ToDecimal(cols[6]);
-                s.Volume = (cols[7].ToUpper().Contains("N/A")) ? (int?) null : Convert.ToInt32(cols[7]);
-                s.High = (cols[8].ToUpper().Contains("N/A")) ? (decimal?)null : Convert.ToDecimal(cols[8]);
-                s.Low = (cols[9].ToUpper().Contains("N/A")) ? (decimal?)null : Convert.ToDecimal(cols[9]);
-                s.High52 = (cols[10].ToUpper().Contains("N/A")) ? (decimal?)null : Convert.ToDecimal(cols[10]);
-                s.Low52 = (cols[11].ToUpper().Contains("N/A")) ? (decimal?)null : Convert.ToDecimal(cols[11]);
-                StockFromApiList.Add(s);
-                } catch (FormatException e)
+                    s.Symbol = Convert.ToString(cols[0]).Trim('"');
+                    s.Name = Convert.ToString(cols[1]).TrimEnd('"');
+                    s.Bid = (cols[2].ToUpper().Contains("N/A")) ? (decimal?)null : Convert.ToDecimal(cols[2]);
+                    s.Ask = (cols[3].ToUpper().Contains("N/A")) ? (decimal?)null : Convert.ToDecimal(cols[3]);
+                    s.Open = (cols[4].ToUpper().Contains("N/A")) ? (decimal?)null : Convert.ToDecimal(cols[4]);
+                    s.PreviousClose = (cols[5].ToUpper().Contains("N/A")) ? (decimal?)null : Convert.ToDecimal(cols[5]);
+                    s.LastTrade = (cols[6].ToUpper().Contains("N/A")) ? (decimal?)null : Convert.ToDecimal(cols[6]);
+                    s.Volume = (cols[7].ToUpper().Contains("N/A")) ? (int?)null : Convert.ToInt32(cols[7]);
+                    s.High = (cols[8].ToUpper().Contains("N/A")) ? (decimal?)null : Convert.ToDecimal(cols[8]);
+                    s.Low = (cols[9].ToUpper().Contains("N/A")) ? (decimal?)null : Convert.ToDecimal(cols[9]);
+                    s.High52 = (cols[10].ToUpper().Contains("N/A")) ? (decimal?)null : Convert.ToDecimal(cols[10]);
+                    s.Low52 = (cols[11].ToUpper().Contains("N/A")) ? (decimal?)null : Convert.ToDecimal(cols[11]);
+                    StockFromApiList.Add(s);
+                }
+                catch (FormatException e)
                 {
                     MessageBox.Show("Unable to conver value", "Confirmation", MessageBoxButton.OK);
                     Console.Write(e.StackTrace);
@@ -70,6 +71,6 @@ namespace TradingApp
         public decimal? High52 { get; set; }
         public decimal? Low52 { get; set; }
 
-        
+
     }
 }
