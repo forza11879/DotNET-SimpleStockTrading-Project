aed431edafed4996c96cad0e6f64a4e824f57a9d
MSI\John <forza11879@gmail.com>
2017-04-28 00:45:06 -0400

    *name and email validation

--- NewPortfolioDialog.xaml.cs
+++ NewPortfolioDialog.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -19,6 +21,7 @@ namespace TradingApp
     /// </summary>
     public partial class NewPortfolioDialog : Window
     {
+
         public NewPortfolioDialog()
         {
             InitializeComponent();
@@ -26,13 +29,65 @@ namespace TradingApp
 
         private void ButtonOK_Click(object sender, RoutedEventArgs e)
         {
+
+
             string name = tbName.Text;
+
+            if (tbName.Text == "" /*|| tbName.Text  2 || tbName.Text > 50*/)
+            {
+                MessageBox.Show(tbName.Text + " Name input canot be empty", "Invalit Input",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+
+            }
+            else
+            {
+
+                
+            }
+
             string email = tbEmail.Text;
+            
+            if (IsValid(tbEmail.Text))
+            {
+                MessageBox.Show(" New Portfolio successfully created", "Message Box");
+
+            }
+            else
+            {
+                MessageBox.Show(tbEmail.Text + " is not a valid email", "Invalit Input",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+
+
             Entities.Portfolio p = new Entities.Portfolio() { Name = name, Email = email };
             Model.DBA_Portfolio.AddNewPortfolioToTable(p);
             //RefreshPortfolios();
             this.DialogResult = true;
         }
+
+        public bool IsValid(string email)
+        {
+            try
+            {
+                MailAddress m = new MailAddress(email);
+
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
+
+        //private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        //{
+        //    Regex regex = new Regex("^[a-zA-Z''-'\s]{1,40}$");
+
+        //    e.Handled = !regex.IsMatch(e.Text);
+        //}
 
 
 
